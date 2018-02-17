a7e2a84ffbd4d026897775ac6f1e5e6e8abcf0c8
Ярослав Дегтярь <yaroslavdehtiar@gmail.com>
2018-02-17 15:24:44 +0200

    working Liked Template

--- src/main/java/ua/danit/ChatServlet.java
+++ src/main/java/ua/danit/ChatServlet.java
@@ -1,24 +1,20 @@
 package ua.danit;
 
 import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 import java.io.PrintWriter;
 
+@WebServlet(urlPatterns = "/chat")
 public class ChatServlet extends HelloServlet{
   UserDao switcher = new UserDao();
 
   @Override
   protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
 
-    PrintWriter writer = resp.getWriter();
-    writer.write("<html><body>");
-    for(User user : switcher.likedUsers()) {
-      writer.write("<form action='/' method=\"post\">");
-      writer.write("<p><img src='"+user.imgUrl+"' width= 100px height= 100px></p>");
-    }
-    writer.write("</body></html>");
+    resp.getWriter().write("HI");
   }
 
   @Override
--- src/main/java/ua/danit/ServletWithLikedPeople.java
+++ src/main/java/ua/danit/ServletWithLikedPeople.java
@@ -7,21 +7,22 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 import java.io.PrintWriter;
+import java.util.Set;
 
 @WebServlet(urlPatterns = "/liked")
 public class ServletWithLikedPeople extends HelloServlet {
   UserDao switcher = new UserDao();
+
   @Override
   protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+    Set<User> users = switcher.likedUsers();
 
-    PrintWriter writer = resp.getWriter();
-    writer.write("<html><body>");
-    for(User user : switcher.likedUsers()) {
-      writer.write("<form action='/liked' method=\"post\">");
-      writer.write("<p><img src='"+user.imgUrl+"' width= 100px height= 100px></p>");
-      writer.write  ("<button type='submit' onClick=localhost:8080/chat src='"+user.id+"'>"+user.name+"</button>");
-    }
-    writer.write("</body></html>");
+//    if(thisUser == null){
+//      resp.sendRedirect("/chat");
+//      return;
+//    }
+    for (User user : users)
+      TemplateForLiked.write(resp.getWriter(), user);
   }
 
   @Override
